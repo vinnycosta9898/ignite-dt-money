a5fd0127066383557e2408cbecb06ad09683f590
Vinicius Costa <viniciuscosta9898@icloud.com>
2023-05-03 19:32:31 -0300

    Criando os arquivos de estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { ThemeProvider } from "styled-components"
+import { GlobalStyle } from "./styles/GlobalStyle"
+import { defaultTheme } from "./styles/themes/default"
+
 function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle/>
       <h1>Hello World</h1>
-    </>
+    </ThemeProvider>
   )
 }
 
