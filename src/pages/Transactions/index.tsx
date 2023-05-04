b6cb64472b7ff67f534283096b654ca4463f7633
Vinicius Costa <viniciuscosta9898@icloud.com>
2023-05-03 22:27:23 -0300

    Criando a tabela de transacoes

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,3 +1,4 @@
+import { PriceHightLight, TransactionContainer, TransactionTable } from './styles'
 import { Header } from "../../components/Header/index";
 import { Summary } from "../../components/Summary/index";
 
@@ -6,6 +7,35 @@ export function Transactions(){
         <div>
             <Header/>
             <Summary/>
+            <TransactionContainer>
+                <TransactionTable>
+                    <tbody>
+                        <tr>
+                            <td width="50%">Desenvolvimento de site</td>
+                            <td>
+                                <PriceHightLight variant="income">
+                                    R$ 12.000,00
+                                </PriceHightLight>
+                            </td>
+                            <td>Venda</td>
+                            <td>12/04/2023</td>
+                        </tr>
+
+                        <tr>
+                            <td width="50%">Hamburguer</td>
+                            <td>
+                                <PriceHightLight variant="outcome">
+                                    R$ 12.000,00
+                                </PriceHightLight>
+                            </td>
+                            <td>Alimentacao</td>
+                            <td>12/04/2023</td>
+                        </tr>
+                        
+                    </tbody>
+                </TransactionTable>
+            </TransactionContainer>
+
         </div>
     )
 }
