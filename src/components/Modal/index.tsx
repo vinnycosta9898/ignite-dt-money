ce57dc062372891244630e627a2151cf4b25101e
Vinicius Costa <viniciuscosta9898@icloud.com>
2023-05-05 21:22:29 -0300

    Criando os Buttons de Entrada e Saida do Modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,6 +1,7 @@
 import * as Dialog from '@radix-ui/react-dialog'
-import { CloseButton, Content, OverLay } from './styles';
+import { CloseButton, Content, OverLay, TransActionType, TransActionTypeButton } from './styles';
 import { X } from 'phosphor-react';
+import { ArrowCircleDown, ArrowCircleUp } from 'phosphor-react';
 
 export function Modal(){
     return(
@@ -14,7 +15,18 @@ export function Modal(){
                 <form action="">
                     <input type="text" placeholder="Descrição" required/>
                     <input type="number" placeholder="Preço" required/>
-                    <input type="text" placeholder="Categoria" required/>
+                    <input type="text" placeholder="Categoria" required/> 
+                    <TransActionType>
+                        <TransActionTypeButton variant="income" value="income">
+                            Entrada
+                            <ArrowCircleUp size={24}/>
+                        </TransActionTypeButton>
+
+                        <TransActionTypeButton variant="outcome" value="outcome">
+                            Saída
+                            <ArrowCircleDown size={24}/>
+                        </TransActionTypeButton>
+                    </TransActionType>
                     <button type="submit">
                         Cadastrar
                     </button>
--- src/components/Modal/styles.ts
+++ src/components/Modal/styles.ts
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 import * as Dialog from '@radix-ui/react-dialog';
+import * as RadioGroup from '@radix-ui/react-radio-group'
+
 
 export const OverLay = styled(Dialog.Overlay)`
     position: fixed;
@@ -30,7 +32,7 @@ export const Content = styled(Dialog.Content)`
     }
 
     input{
-        background: ${(props) => props.theme["gray-900"]}
+        background: ${(props) => props.theme["gray-900"]};
         border-radius: 6px;
         border: 0;
         color: ${(props) => props.theme["gray-300"]};
@@ -69,5 +71,50 @@ export const CloseButton = styled(Dialog.Close)`
     background: transparent;
     border: 0;
     cursor: pointer;
-    color: ${(props) => props.theme["gray-500"]}
+    color: ${(props) => props.theme["gray-500"]};
 `;
+
+export const TransActionType = styled(RadioGroup.Root)`
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 1rem;
+    margin-top: 0.5rem;
+`
+
+interface TransActionTypeProps{
+    variant: "income" | "outcome";
+}
+
+export const TransActionTypeButton = styled(RadioGroup.Item)<TransActionTypeProps>`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 0.5rem;
+    
+    background: ${(props) => props.theme["gray-700"]};
+    border: 0;
+    border-radius: 6px;
+    cursor: pointer;
+    color: ${(props) => props.theme["gray-300"]};
+    padding: 1rem;
+
+    svg{
+        color: ${props => props.variant === "income" ? props.theme["green-300"] : props.theme["red-300"]};
+    }
+
+    &[data-state="checked"]{
+        background: ${props => props.variant === "income" ? props.theme["green-500"] : props.theme["red-500"]};
+        color: ${(props) => props.theme.white};
+
+        svg{
+            color: ${(props) => props.theme.white}
+        }
+    }
+
+    &[data-state="unchecked"]:hover{
+        background: ${(props) => props.theme["gray-600"]};
+        transition: background-color 0.2s;
+    }
+
+
+`
